02c6e4e0bfc7e7d24620d5639da1ea75a02a475b
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-11-14 17:48:41 +0545

    Add menu module

---
+++ modules/menu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void showMenu(){
+    printf("==================\n");
+    printf("=======Menu=======\n");
+    printf("==================\n");
+    printf("1. Register\n");
+    printf("2. Login\n");
+    printf("3. Exit\n");
+    printf("==================\n");
+}
