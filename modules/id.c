ba899aab459f4d96642d422008bfb4cc735a3eb8
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-11-14 13:23:20 +0545

    Create header file for id module

--- modules/id.c
+++ modules/id.c
@@ -1,4 +1,8 @@
+#include <time.h>
+#include <stdlib.h>
+#include "id.h"
+
 int generateId() {
     srand(time(NULL));
     return rand() % 100000000000000;
-    }
+}
---
+++ modules/id.h
@@ -0,0 +1,6 @@
+#ifndef ID_H_   /* Include guard */
+#define ID_H_
+
+int generateId();  /* An example function declaration */
+
+#endif // ID_H_
