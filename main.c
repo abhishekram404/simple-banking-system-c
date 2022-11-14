8d8da836b11fd0756274c3e2e1d758ccf1aee154
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-11-14 17:49:02 +0545

    Input user's choice and execute respective function

--- main.c
+++ main.c
@@ -3,12 +3,26 @@
 #include "modules/id.c"
 #include "modules/customer.c"   
 #include "modules/auth.c"
-
+#include "modules/menu.c"
 void main(){
+    int choice;
+    showMenu();
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
 
-    // registerCustomer();
-    // displayCustomers();
-    login();
+    switch(choice){
+        case 1:
+            registerCustomer();
+            break;
+        case 2:
+            login();
+            break;
+        case 3:
+            exit(0);
+            break;
+        default:
+            printf("Invalid choice");
+    }
 
 printf("\nPress ENTER to exit");
 char a;
