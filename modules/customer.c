322f72c89bf6d7c9a2e77ad030928585b1b44386
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-11-14 12:50:35 +0545

    Add customer struct in customer module

---
+++ modules/customer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// customer struct
+struct Customer{
+    int id;
+    char name[50];
+    int age;
+    char address[100];
+    char phone[20];
+    float balance ;
+    char password[20];
+    char username[20];
+    char email[50];
+};
